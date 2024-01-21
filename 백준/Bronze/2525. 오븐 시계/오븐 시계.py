1149788af14fef723086eeb986472eb32688d663
minyeong981 <156265474+minyeong981@users.noreply.github.com>
2024-01-21 17:34:14 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,11 @@
 A, B = map(int, input().split())
 C = int(input())
 
-A = A + C//60
-B = B + C%60
-
-if B >= 60 :
-    A = A+1
-    B = B-60
-if A > 23 :
-    A = A-24
-print(A, B)
+if B+C >= 60 :
+    A = A + ((B+C)//60)
+    B = ((B+C)%60)
+    if A > 23 :
+        A = A%24
+    print(A,B)
+else:
+    print(A, B+C)
