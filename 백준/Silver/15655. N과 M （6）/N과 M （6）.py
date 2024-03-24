1ff0658e04241b4b7756f3f93d4afbe88ab64ba9
minyeong981 <156265474+minyeong981@users.noreply.github.com>
2024-03-24 15:43:49 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,14 @@
+def combi(k,lst):
+    if len(lst) == M :
+        print(*lst)
+        return
+
+    for i in range(k, N):
+        lst.append(A[i])
+        combi(i+1, lst)
+        lst.pop()
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+A.sort()
+combi(0,[])
