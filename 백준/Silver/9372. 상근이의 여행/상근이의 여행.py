84bf01a520cbfada1bdd40c21b7a78b06ade972c
minyeong981 <156265474+minyeong981@users.noreply.github.com>
2024-04-20 23:07:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 192 ms, Memory: 110476 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,30 +1,8 @@
-def dfs(s):
-    st = []
-    st.append(s)
-    visited = [False] * (N + 1)
-    visited[s] = True
-    cnt = 0
-
-    while st:
-        v = st.pop()
-        for w in G[v]:
-            if not visited[w]:
-                st.append(w)
-                visited[w] = True
-                cnt += 1
-
-    return cnt
-
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
 
-    G = [[] for _ in range(N + 1)]
     for _ in range(M):
         a, b = map(int, input().split())
-        G[a].append(b)
-        G[b].append(a)
-    #print(G) [[], [2, 3], [1, 3], [2, 1]]
 
-    Cnt = dfs(1)
-    print(Cnt)
+    print(N-1)
