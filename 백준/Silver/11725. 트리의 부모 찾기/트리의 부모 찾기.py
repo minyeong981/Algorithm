3b877b78ffefb1bd39ae2a3c60c99ae562ab5441
minyeong981 <156265474+minyeong981@users.noreply.github.com>
2024-04-21 11:55:12 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 260 ms, Memory: 123960 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,30 +1,32 @@
-from collections import deque
-
-def build_tree(root):
-    q = deque()
-    q.append(root)
-    visited = [False] * (N + 1)
+def dfs(root) :
+    st = []
+    st.append(root)
+    visited = [False] * (N+1)
     visited[root] = True
 
-    while q:
-        v = q.popleft()
+    while st :
+        v = st.pop()
+
         for w in G[v]:
-            if not visited[w]:
+            if not visited[w] :
+                st.append(w)
                 tree[w] = v
-                q.append(w)
                 visited[w] = True
-    # print(tree)
+
+
 
 N = int(input())
-G = [[] for _ in range(N + 1)]
-tree = [0] * (N + 1)
 
-for _ in range(N - 1):
-    a, b = map(int, input().split())
-    G[a].append(b)
-    G[b].append(a)
+G = [[] for _ in range(N+1)]
+tree = [0] * (N+1)
+# TREE = [[] for _ in range(N+1)]
+for _ in range(N-1) :
+    v1, v2 = map(int, input().split())
+    G[v1].append(v2)
+    G[v2].append(v1)
+#print(G) [[], [6, 4], [4], [6, 5], [1, 2, 7], [3], [1, 3], [4]]
 
-build_tree(1)
+dfs(1)
 
-for i in range(2, N + 1):
+for i in range(2, N+1) :
     print(tree[i])
