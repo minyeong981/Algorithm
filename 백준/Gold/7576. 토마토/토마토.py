632b92b4858fbf82d614752ded1f15cb9ac77b25
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-01-14 16:58:19 +0900

    [Gold V] Title: 토마토, Time: 392 ms, Memory: 156272 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,42 +1,35 @@
 from collections import deque
 
-import sys
-input = sys.stdin.readline
+def bfs() :
+    while q :
+        vr, vc = q.popleft()
+        for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)] :
+            nr = vr + dr
+            nc = vc + dc
+            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0 :
+                q.append((nr, nc))
+                arr[nr][nc] = arr[vr][vc] + 1
 
 M, N = map(int, input().split())
-box = [list(map(int, input().split())) for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-def findMinDay() :
-    while tomatoes :
-        xr, xc = tomatoes.popleft()
+q = deque()
+for row in range(N) :
+    for col in range(M) :
+        if arr[row][col] == 1 :
+            q.append((row,col))
 
-        for dr, dc in [(0, 1), (1, 0),(0, -1), (-1, 0)] :
-            nr = xr + dr
-            nc = xc + dc
-            if 0 <= nr < N and 0 <= nc < M and box[nr][nc] == 0 :
-                box[nr][nc] = box[xr][xc] + 1
-                tomatoes.append((nr, nc))
+bfs()
 
-tomatoes = deque([])
-cnt = 0
-for r in range(N) :
-    for c in range(M) :
-        if box[r][c] == 1 :
-            tomatoes.append((r,c))
-            cnt += 1
+maxV = 0
+is_zero = False
+for row in range(N) :
+    for col in range(M) :
+        if arr[row][col] == 0 :
+            is_zero = True
+        maxV = max(maxV, arr[row][col])
 
-if cnt == M*N :
-    print(0)
-    exit()
-else:
-    findMinDay()
-
-maxDay = 0
-for r in range(N) :
-    for c in range(M) :
-        if box[r][c] == 0 :
-            print(-1)
-            exit()
-        else :
-            maxDay = max(maxDay, box[r][c])
-print(maxDay - 1)
+if is_zero :
+    print(-1)
+else :
+    print(maxV-1)
