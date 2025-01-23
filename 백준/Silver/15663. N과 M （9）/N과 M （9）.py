0a34118561d3ab242cffe7a979eb0c45e0b797e4
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-01-23 21:44:36 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 114516 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,21 +1,12 @@
-def perm(k, lst):
-    if len(lst) == M :
-        result.add(tuple(lst.copy()))
-        return
+from itertools import permutations
 
-    for i in range(N):
-        if not visited[i]:
-            lst.append(A[i])
-            visited[i] = True
-            perm(k+1, lst)
-            lst.pop()
-            visited[i] = False
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
-visited = [False for _ in range(N)]
-result = set()
-perm(0, [])
+lst = list(map(int, input().split()))
+# lst.sort()
 
-for ans in sorted(result) :
-    print(' '.join(map(str, ans)))
+result = set(permutations(lst, M))
+for ele in sorted(result) :
+    print(*ele)
