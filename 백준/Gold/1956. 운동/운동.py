d81a371a49c2288a678d2f53c4bd825ac5fb352b
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-02-28 22:25:23 +0900

    [Gold IV] Title: 운동, Time: 1748 ms, Memory: 113332 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -2,13 +2,15 @@ import sys
 input = sys.stdin.readline
 
 def solve() :
-    result = INF
     for k in range(1, V+1) :
         for i in range(1, V+1) :
             for j in range(1, V+1) :
                 if G[i][k] != INF and G[k][j] != INF :
                     G[i][j] = min(G[i][j], G[i][k] + G[k][j])
-                    result = min(result, G[i][j] + G[j][i])
+    result = INF
+    for path in range(1, V + 1) :
+        if result > G[path][path] :
+            result = G[path][path]
 
     if result != INF :
         return result
