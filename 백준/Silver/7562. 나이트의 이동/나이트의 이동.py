9873cf78018fc0e93ffc8bebd828f1daf8eebff3
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-03-17 02:13:02 +0900

    [Silver I] Title: 나이트의 이동, Time: 272 ms, Memory: 131028 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 def bfs(x, y, targetX, targetY) :
     q = deque()
     q.append((x, y))
-    visited = [[float('inf')] * I for _ in range(I)]
+    visited = [[0] * I for _ in range(I)]
     visited[x][y] = 0
     while q :
         cuX, cuY = q.popleft()
@@ -16,16 +16,9 @@ def bfs(x, y, targetX, targetY) :
         for dr, dc in [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (2, -1), (2, 1), (1, -2), (1, 2)] :
             newX = cuX + dr
             newY = cuY + dc
-            if 0 <= newX < I and 0 <= newY < I and visited[newX][newY] == float('inf') :
+            if 0 <= newX < I and 0 <= newY < I and visited[newX][newY] == 0 :
                 visited[newX][newY] = visited[cuX][cuY] + 1
                 q.append((newX, newY))
-
-
-def solve(moveR, moveC) :
-    for r in range(I) :
-        for c in range(I) :
-            if (r, c) == (currR, currC) :
-                return bfs(r, c, moveR, moveC)
 
 
 t = int(input().strip())
@@ -33,4 +26,4 @@ for _ in range(t) :
     I = int(input().strip())
     currR, currC = map(int, input().split())
     moveR, moveC = map(int, input().split())
-    print(solve(moveR, moveC))
+    print(bfs(currR, currC, moveR, moveC))
