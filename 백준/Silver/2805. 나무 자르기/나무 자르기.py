9343cfb763a1adbb8fd6f1ac95243c83bcde1acc
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-03-29 23:34:39 +0900

    [Silver II] Title: 나무 자르기, Time: 444 ms, Memory: 258360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 tree = list(map(int, input().split()))
 tree.sort()
-s, e = 1, tree[-1]
+s, e = 0, tree[-1]
 maxh = 0
 while s <= e :
     mid = (s + e) // 2
