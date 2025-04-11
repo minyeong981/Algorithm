c328508e72568ccbd510aebd77f503958e675e5e
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-04-11 13:08:33 +0900

    [Silver IV] Title: 설탕 배달, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,12 @@
-import sys
+N = int(input())
 
-N = int(sys.stdin.readline().strip())
-dp = [float('inf')] * (N + 1)
-dp[0] = 0  # 0kg을 만들기 위한 최소 봉지 수는 0
-
-for i in range(N + 1):
-    if dp[i] == float('inf'):
-        continue
-    if i + 3 <= N:
-        dp[i + 3] = min(dp[i + 3], dp[i] + 1)
-    if i + 5 <= N:
-        dp[i + 5] = min(dp[i + 5], dp[i] + 1)
-
-print(dp[N] if dp[N] != float('inf') else -1)
+cnt = 0
+while N >= 0:
+    if N % 5 == 0:
+        cnt += N // 5
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+else:
+    print(-1)
