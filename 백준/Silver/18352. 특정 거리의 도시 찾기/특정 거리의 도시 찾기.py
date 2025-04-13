f10f21246606f8a84a54f603ef4de66f8b0b5b07
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-04-13 18:59:10 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 816 ms, Memory: 154816 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,36 +1,33 @@
-import heapq
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-def solve(X) :
-    q = []
-    heapq.heappush(q, (0, X))
-    visited = [float('inf')] * (N + 1)
+def solve(X, K) :
+    q = deque([X])
+    visited = [-1] * (N + 1)
     visited[X] = 0
     while q :
-        dist, city = heapq.heappop(q)
+        city = q.popleft()
 
-        if visited[city] < dist :
+        if visited[city] > K : # 시간 단축 코드
             continue
 
-        for nextDist, nextC in path[city] :
-            newDist = dist + nextDist
-            if visited[nextC] > newDist :
-                visited[nextC] = newDist
-                heapq.heappush(q, (newDist, nextC))
-                
+        for nextC in path[city] :
+            if visited[nextC] == -1 and visited[nextC] + 1 < K :
+                visited[nextC] = visited[city] + 1
+                q.append(nextC)
+
     return visited
 
 N, M, K, X = map(int, input().split())
 path = [[] for _ in range(N + 1)]
 for _ in range(M) :
     A, B = map(int, input().split())
-    path[A].append((1, B))
-arr = solve(X)
+    path[A].append(B)
+arr = solve(X, K)
 
 result = []
 for i in range(1, N + 1) :
     if arr[i] == K :
         result.append(i)
-        
 print('\n'.join(map(str, sorted(result))) if len(result) != 0 else -1)
