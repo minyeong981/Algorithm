8b1a6820cda80ceaf3aa61aba238890ed19ad765
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-06-06 15:53:27 +0900

    [level 2] Title: 주식가격, Time: 32.24 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,15 @@
 def solution(prices):
-    answer = []
     n = len(prices)
+    answer = [0] * n
+    stack = []
+    
     for i, price in enumerate(prices) :
-        j = i
-        while j < n-1 :
-            if price > prices[j] :
-                break
-            j += 1
-        answer.append(j-i)
+        while stack != [] and stack[-1][1] > price :
+            j, _ = stack.pop()
+            answer[j] = i - j
+        stack.append([i, price])
+
+    for i, price in stack :
+        answer[i] = n - i - 1
             
     return answer
