e853bea524a698313c3946fd70eeaa7f4ae98e73
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-06-20 14:22:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.84 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,12 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let numer = (numer1*denom2) + (numer2*denom1);
+    let denom = denom1*denom2;
+    for (let i = Math.min(numer, denom); i > 0; i--) {
+        if (numer % i === 0 && denom % i === 0) {
+            numer /= i;
+            denom /= i;
+        }
+    }
+    
+    return [numer, denom];
+}
