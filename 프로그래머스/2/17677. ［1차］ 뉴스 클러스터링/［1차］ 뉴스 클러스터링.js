e0e5efd4e4f22f77fdcbf2ac0c64311463f1528e
Minyeong Kim <156265474+minyeong981@users.noreply.github.com>
2025-07-06 15:50:25 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.33 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,36 @@
+function makeValidElements(str) {
+    const validMap = new Map();
+    let validEleLength = 0;
+    const charArray = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        let ele = charArray.slice(i, i+2).join('').toLowerCase();
+        if (/^[a-z]{2}$/.test(ele)) {
+            validMap.set(ele, (validMap.get(ele) || 0) + 1);
+            validEleLength++;
+        }
+    }
+    return [validMap, validEleLength];
+}
+
+function solution(str1, str2) {
+    const [s1Map, s1Size] = makeValidElements(str1);
+    const [s2Map, s2Size] = makeValidElements(str2);
+
+    if (s1Size === 0 && s2Size === 0) return 65536;
+    
+    // 교집합 수
+    let intersectionCount = 0;
+    for (const [key, value] of s2Map) {
+        if (s1Map.has(key)) {
+            const count = Math.min(s1Map.get(key), value);
+            for (let i = 0; i < count; i++) {
+                intersectionCount++;
+            }
+        }
+    }
+    
+    // 합집합 수
+    const unionCount = s1Size + s2Size - intersectionCount;
+
+    return Math.trunc(intersectionCount/unionCount*65536);
+}
